42704e5e3f058e70e276488c4a3d3bcdd5d7a7a2
estshorter <1430311+estshorter@users.noreply.github.com>
2021-08-25 19:32:11 +0900

    refactoring

--- examples/main.cpp
+++ examples/main.cpp
@@ -1,35 +1,53 @@
-#include <chrono>
 #include <iostream>
 #include <mlflow.hpp>
+#include <chrono>
+#include <thread>
 
-int main(void) {
+cpp::result<void, std::string> example_low_level(void) {
 	using namespace std::chrono;
-
 	mlflow::Client cli("http://localhost:5000");
 	auto exp = cli.get_experiment_by_name("Default");
 	if (!exp) {
-		std::cerr << exp.error() << std::endl;
-		return 1;
+		return cpp::failure(exp.error());
 	}
 	std::string exp_id = exp.value().experiment_id;
 	auto run = cli.create_run(exp_id);
 	if (!run) {
-		std::cerr << run.error() << std::endl;
-		return 1;
+		return cpp::failure(run.error());
 	}
 	std::string run_id = run.value().info.run_id;
-	mlflow::Param param{"KEY", "KEY_VALUE"};
-	auto ret = cli.log_param(run_id, param);
+	cli.set_runid(run_id);
+
+	mlflow::Param param{"PARAM1", "PARAM_VALUE1"};
+	auto ret = cli.log_param(param);
+	if (!ret) {
+		return cpp::failure(ret.error());
+	}
+	ret = cli.set_tag({"TAG1", "TAG_VALUE1"});
+	if (!ret) {
+		return cpp::failure(ret.error());
+	}
+	ret = cli.log_batch({}, {{"PARAM2", "PARAM_VALUE2"}}, {{"TAG2", "TAG_VALUE2"}});
+	if (!ret) {
+		return cpp::failure(ret.error());
+	}
+	ret = cli.log_metric({"METRIC_KEY", "0.0", 0});
+	std::this_thread::sleep_for(1s);
+	ret = cli.log_metric({"METRIC_KEY", "1.0", 1});
+	if (!ret) {
+		return cpp::failure(ret.error());
+	}
+	auto ret_ = cli.update_run(mlflow::RunStatus::FINISHED);
+	if (!ret_) {
+		return cpp::failure(ret_.error());
+	}
+	return {};
+}
+
+int main(void) {
+	auto ret = example_low_level();
 	if (!ret) {
 		std::cerr << ret.error() << std::endl;
-		return 1;
-	}
-	ret = cli.set_tag(run_id, {"TAG", "TAG_VALUE"});
-	ret = cli.log_batch(run_id, {}, {{"KEY1", "VALUE"}}, {{"KEY2", "VALU2"}});
-
-	auto ret_ = cli.update_run(run_id, mlflow::RunStatus::FINISHED);
-	if (!ret_) {
-		std::cerr << ret_.error() << std::endl;
 		return 1;
 	}
 	return 0;
